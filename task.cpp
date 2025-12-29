2adb3e51fb484df83d5cd87aa4a495dc9f717bbd
Aman Chaudhary <amanc3147@gmail.com>
2025-12-29 17:36:53 +0530

    added validations for valid operators usage

--- task.cpp
+++ task.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int precedence(char op){
     if(op=='+' || op=='-')return 1;
+
     if(op=='*' || op=='/')return 2;
+
     return 0;
 }
 
@@ -22,21 +24,33 @@ double applyOperation(double a,double b,char op){
 }
 double solve(const string& exp){
     stack<double>values;
+
+
     stack<char>ops;
+
+
     stringstream ss(exp);
+
     double num;
+
     char ch;
+
     while(ss >> ws){
         if(isdigit(ss.peek())){
+
             ss>>num;
+
             values.push(num);
         }else{
             ss>>ch;
             while(!ops.empty() && precedence(ops.top()) >= precedence(ch)){
                 double b=values.top();
+
                 values.pop();
+
                 double a=values.top();
                 values.pop();
+                
                 values.push(applyOperation(a,b,ops.top()));
                 ops.pop();
             }
@@ -45,21 +59,48 @@ double solve(const string& exp){
     }
     while(!ops.empty()){
         double b=values.top();
+
         values.pop();
+
         double a=values.top();
+
         values.pop();
+
         values.push(applyOperation(a,b,ops.top()));
         ops.pop();
     }
     return values.top();
 }
+bool isValidExpression(string& exp){
+    for(char ch:exp){
+
+        if(!isdigit(ch) && ch!='+' && ch!='-' && ch!='*' && ch!='/' && ch!=' ')
+        return false;
+    }
+    return true;
+}
+
+
+
+
 int main(){
     string exp;
+
     cout<<"Enter Expression: ";
+
     getline(cin,exp);
+    if(!isValidExpression(exp)){
+
+        cout<<"Invalid character in expression";
+
+        return 0;
+    }
+ 
     try{
         cout<<"Result: "<<solve(exp);
+
     }catch(const exception& e){
+
         cout<<"Error: "<<e.what();
     }
     return 0;
